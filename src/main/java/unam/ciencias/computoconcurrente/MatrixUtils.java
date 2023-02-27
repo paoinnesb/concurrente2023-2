c0664aa79acac1e9c8334f3415707e17e81dc7f8
paoinnesb <paoinnes@ciencias.unam.mx>
2023-02-27 16:16:55 -0600

    matrix average ready

--- src/main/java/unam/ciencias/computoconcurrente/MatrixUtils.java
+++ src/main/java/unam/ciencias/computoconcurrente/MatrixUtils.java
@@ -1,43 +1,68 @@
 package unam.ciencias.computoconcurrente;
-
+import java.util.Arrays;;
 
 public class MatrixUtils implements Runnable{
     private int threads;
-    private static int[] posiblesPromedios; // Arreglo para que cada hilo guarde su minimo encontrado
-    private static int[] matrixGlobal; 
-    private static int secciones; 
+    private double[] posiblesPromedios; // Arreglo para que cada hilo guarde su minimo encontrado
+    private int[][] matrixGlobal;
+    private int indice;
 
     public MatrixUtils() {
         this.threads = 1;
+        this.posiblesPromedios = new double[1];
     }
 
     public MatrixUtils(int threads) {
         this.threads = threads;
+        this.posiblesPromedios = new double[threads];
+    }
+
+    public MatrixUtils(int indice, double[] promedios, int[][] matrixGlobal){
+        this.posiblesPromedios = promedios;
+        this.indice = indice;
+        this.matrixGlobal = matrixGlobal;
     }
 
     @Override
     public void run() {
-        
+        double prom = this.Average(matrixGlobal);
+        this.posiblesPromedios[this.indice] = prom;
     }
 
-    private void setMatrixGlobal(int[] matrix){
-        this.matrixGlobal = matrix;
-        return;
-    }
 
     public double findAverage(int[][] matrix) throws InterruptedException{
-
+        int divisiones = (matrix.length - (matrix.length % this.threads)) / this.threads;
+        
         Thread[] threads = new Thread[this.threads];
         
         for (int i = 0; i < this.threads; i++) {
-            MatrixUtils p = new MatrixUtils();
+            int[][] div;
+            if((matrix.length % this.threads) != 0 && i == this.threads - 1){
+                div = Arrays.copyOfRange(matrix, (i*divisiones), matrix.length);
+            } else{
+                div = Arrays.copyOfRange(matrix, (i * divisiones), (i * divisiones) + divisiones);
+            }
+            MatrixUtils p = new MatrixUtils(i, this.posiblesPromedios, div);
             threads[i] = new Thread(p);
         }
 
-        
+        for (Thread thread : threads) {
+            thread.start();
+        }
 
-        return 1.0;
-       
+        for (Thread thread : threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                System.out.println("Ocurrió una excepción al esperar a que los threads terminen su ejecución.");
+                e.printStackTrace();
+            }
+        }
+        double p = 0;
+        for (int i = 0; i < this.threads; i++) {
+            p += this.posiblesPromedios[i];
+        }
+        return p / this.posiblesPromedios.length;
     }
 
     /**
